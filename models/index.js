972cae4a07b5eb086ea2d3f22fd41c860b073ef7
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2022-06-01 18:35:29 -0500

    delete cascade

--- models/index.js
+++ models/index.js
@@ -6,26 +6,30 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'category_id'
+  foreignKey: 'category_id',
+  onDelete: 'cascade'
 })
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'category_id'
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
 })
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
   as: 'product_tags',
-  foreignKey: 'product_id'
+  foreignKey: 'product_id',
+  onDelete: 'cascade'
 })
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
   as: 'tags',
-  foreignKey: 'tag_id'
+  foreignKey: 'tag_id',
+  onDelete: 'cascade'
 })
 
 module.exports = {
